c43e4395fcb751b8494b985477f04915fe0f71fe
Jake Robins <mendahu@gmail.com>
2019-12-18 16:11:16 +0000

    added js file to conduct draw, updated eslint for es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   'extends': 'lighthouselabs',
+  "parserOptions": {
+    "ecmaVersion": 6
+  }
 };
---
+++ chart-draw.js
@@ -0,0 +1,3 @@
+const drawBarChart = function(data, options, element) {
+  console.log("Hello World");
+};
