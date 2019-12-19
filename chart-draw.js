fb3a8fd49155eb1f5cdeb7cd066a788f301eb165
Jake Robins <mendahu@gmail.com>
2019-12-19 02:10:21 +0000

    initial work on having chartDraw function create graph using DOM elements and stylizing them

--- chart-draw.js
+++ chart-draw.js
@@ -40,27 +40,32 @@ const drawBarChart = function(data, options, element) {
   //ProcessedOptions will be used going forward
   let processedOptions = optionsCleaner(Object.assign(defaultOptions, options));
 
-  console.log("The data points submitted are: ");
-  console.log(data);
-  console.log("The Default options are: ");
-  console.log(defaultOptions);
-  console.log("The Gathered options are: ");
-  console.log(gatheredOptions);
-  console.log("The Processed options are: ");
-  console.log(processedOptions);
-
-
-  //console.log(element);
-
   //Determines a base unit of width for charting the bars, based on how many data points were passed
   //Narrow spacing makes the spaces half the width of a bar
   //Even spacing makes the spaces the same as the bar
   //Wide spacing makes the spaces 150% the width of a bar
-  let barWidthUnit = (data.length * 2) + ((data.length + 1) * processedOptions.barSpacing);
+  let widthUnit = (data.length * 2) + ((data.length + 1) * processedOptions.barSpacing);
 
-  console.log(element);
+  //Go to work creating the graph DOM elements
+  //This creates the Chart div which is where the bars live and sets its height to the width of its container
+  element.append("<div id=\"chart\"></div>");
+  //$("#chart");
 
-  $("#chart-container").append("<div id=\"chart\"></div>");
+  //This creates vertical columns that represent spaces and bars
+  for (let i = 1; i <= (data.length); i++) {
+    $("#chart").append("<div id=\"space" + i + "\" class=\"chartSpace chartColumn\"></div>");
+    $("#chart").append("<div id=\"bar" + i + "\" class=\"chartBar chartColumn\"></div>");
+  }
+  $("#chart").append("<div id=\"space" + (data.length + 1) + "\" class=\"chartSpace\"></div>");
+
+  //Sets vertical columbs of spaces and bars to flex-grow equally, but set their ratios based on user option
+  $("#chart").children().css("flex-grow", "1");
+  //$("#chart").children(".chartSpace").css("flex-basis", (barSpacing / widthUnit);
+  $("#chart").children(".chartBar").css("flex-basis", (2 / widthUnit));
+
+  //Breaks up vertical columns with bars of data into a data row and a empty space row above it
+  $(".chartBar").append("<div class=\"chartBarData\"></div><div class=\"chartBarVerticalSpace\"></div>");
+
 
 };
 
