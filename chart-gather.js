8194395688ea9bab70e15b40f0eae4bb2428b139
Jake Robins <mendahu@gmail.com>
2019-12-29 17:37:04 +0000

    completed layout for data form, completed helper function to add lines as needed

--- chart-gather.js
+++ chart-gather.js
@@ -1,7 +1,135 @@
 $(document).ready(function() {
+  //Creates the data input line by calling the Line adding function
+  lineAdder();
+
+  //Hides the first line item remover so it can't be removed
+  $("#input-section-data-form-line-item-1").children().first().css("visibility", "hidden");
+
   //Adds the onclick listener to the generate button.
   $("#data-submit").attr("onclick", "gatherData()");
 });
+
+
+
+const lineAdder = function() {
+  //For convenience
+  let $form = $("#input-section-data-form");
+
+  //Remove Adder line first. This is re-populated at the end of the function
+  let $currentLine = $form.children().last();
+  $currentLine.remove();
+
+  //Figures out how many lines already exist so it knows what to label new lines
+  let lineCount = $form.children().length;
+  console.log(lineCount);
+
+
+
+
+
+  // ******************************************************************
+  // This section adds a new line to the data form
+  // ******************************************************************
+
+  //Create new line for data
+  //Creates container for entire line
+  $form.append("<div id=\"input-section-data-form-line-item-" + (lineCount + 1 ) + "\" class=\"input-section-data-form-line\"></div>");
+  $currentLine = $form.children().last();
+
+  // Populate the new line with different cells
+
+  //Creates a cell to hold the button that can later remove this line
+  $currentLine.append("<div id=\"line-item-remover-button-" + (lineCount + 1 ) + "\"></div>");
+  let $lineItemModifierCSS = {
+    "width": "1.6em",
+    "height": "1.6em",
+    "border-radius": "10px"
+  };
+  let $lineItemRemoverCSS = {
+    "border": "2px solid red"
+  };
+  let $currentLineCell = $currentLine.children().last();
+  $currentLineCell.css($lineItemModifierCSS);
+  $currentLineCell.css($lineItemRemoverCSS);
+
+  //Populates the grid cell for the line item remover with the button to do so
+  $currentLineCell.append("<span class=\"line-item-remover-text\" id=\"line-item-remover-" + (lineCount + 1 ) + "\">-</span>");
+  let $lineItemModifierTextCSS = {
+    "font-size": "1.6em",
+    "margin": "auto",
+    "display": "block",
+    "text-align": "center"
+  };
+  let $lineItemRemoverTextCSS = {
+    "color": "red"
+  };
+  $currentLineItemCellLastChild = $currentLineCell.children().last();
+  $currentLineItemCellLastChild.css($lineItemModifierTextCSS);
+  $currentLineItemCellLastChild.css($lineItemRemoverTextCSS);
+
+  //lineCount = $form.children().length;
+  $("#line-item-remover-button-" + (lineCount + 1)).attr("onclick", "lineRemover(" + (lineCount + 1) + ")");
+
+
+  //Create and populate a cell for the data label
+  $currentLine.last().append("<div class=\"line-item-label\"><span class=\"input-section-data-form-text\">Data Bar " + (lineCount + 1 ) + " Label: </span><input type=\"text\" name=\"line-item-label-" + (lineCount + 1 ) + "\" class=\"input-section-field-box\" size=\"40\"></div>");
+
+  //Create and populate cells for up to five data per bar
+  $currentLine.last().append("<div class=\"line-item-data-field\"><span class=\"input-section-data-form-text\">Data Bar " + (lineCount + 1 ) + " Data Points: </span><input type=\"text\" name=\"line-item-data-field-" + (lineCount + 1 ) + "-1\" class=\"input-section-field-box\" size=\"5\"></div>");
+  $currentLine.last().append("<div class=\"line-item-data-field\"><input type=\"text\" name=\"line-item-data-field-" + (lineCount + 1 ) + "-2\" class=\"input-section-field-box\" size=\"5\"></div>");
+  $currentLine.last().append("<div class=\"line-item-data-field\"><input type=\"text\" name=\"line-item-data-field-" + (lineCount + 1 ) + "-3\" class=\"input-section-field-box\" size=\"5\"></div>");
+  $currentLine.last().append("<div class=\"line-item-data-field\"><input type=\"text\" name=\"line-item-data-field-" + (lineCount + 1 ) + "-4\" class=\"input-section-field-box\" size=\"5\"></div>");
+  $currentLine.last().append("<div class=\"line-item-data-field\"><input type=\"text\" name=\"line-item-data-field-" + (lineCount + 1 ) + "-5\" class=\"input-section-field-box\" size=\"5\"></div>");
+
+  //Styles all cells with standard CSS
+  let $lineItemCellCSS = {
+    "margin": "5px 10px 5px 10px"
+  };
+  $currentLine.children().css($lineItemCellCSS);
+
+
+
+  // ******************************************************************
+  // This section adds an add button to the data form to continue adding more lines
+  // ******************************************************************
+
+  //Create new line for Add Button
+
+  //Creates container for another entire line
+  $form.append("<div id=\"input-section-data-form-line-item-" + (lineCount + 2 ) + "\" class=\"input-section-data-form-line\"></div>");
+  $currentLine = $form.children().last();
+
+  //Creates a grid cell to hold the button that can add another line
+  $currentLine.append("<div id= \"line-item-adder-button\" class=\"line-item-adder\"></div>");
+  let $lineItemAdderCSS = {
+    "border": "2px solid green"
+  };
+  $currentLineCell = $currentLine.children().last();
+  $currentLineCell.css($lineItemModifierCSS);
+  $currentLineCell.css($lineItemAdderCSS);
+  $currentLineCell.css($lineItemCellCSS);
+
+  $("#line-item-adder-button").attr("onclick", "lineAdder()");
+
+
+  //Populates the grid cell for the line item adder with the button to do so
+  $currentLineCell.append("<span class=\"line-item-adder-text\" id=\"line-item-adder-" + (lineCount + 2 ) + "\">+</span>");
+  $lineItemModifierTextCSS = {
+    "font-size": "1.6em",
+    "margin": "auto",
+    "display": "block",
+    "text-align": "center"
+  };
+  let $lineItemAdderTextCSS = {
+    "color": "green"
+  };
+  $currentLineItemCellLastChild = $currentLineCell.children().last();
+  $currentLineItemCellLastChild.css($lineItemModifierTextCSS);
+  $currentLineItemCellLastChild.css($lineItemAdderTextCSS);
+
+};
+
+
 
 const styler = function() {
   let chartAxesLabel = {
